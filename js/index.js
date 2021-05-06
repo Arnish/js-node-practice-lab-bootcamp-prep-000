631158812bd703a53a16a317c29befbf39f07fbf
Arnish Shakil <arnish.shakil@gmail.com>
2021-05-06 19:20:35 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@
 var name = "Joe"
 var height = "74"
 
-console.log("Name:")
+console.log("Name:A")
 console.log(name)
 
-console.log("Height:")
-console.log(height)
+console.log("Height:5ft6in")
+console.log(height) 
